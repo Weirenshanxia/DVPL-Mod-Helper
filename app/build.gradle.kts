91f5a44082a7b61d9e9dbf286f2f87aa2e107b7e
Weirenshanxia <Weirenshanxia@users.noreply.github.com>
2026-09-14 19:06:42 +0800

    忘改版本号了（

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dvpl.modhelper"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         
         // 只支持 arm64-v8a，大幅减小体积
         ndk {
